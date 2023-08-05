90c03038e49fc85b76e091168e5b02d76eaa87a3
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-05 09:23:51 -1000

    [FEATURE] header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Haeder from "./components/Haeder"
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className="w-11/12 max-w-lg mt-10 mx-auto px5 py-10 shadow-lg rounded-md bg-white">
+      <Haeder />
+    </div>
   )
 }
 
---
+++ src/components/Haeder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Haeder = () => {
+  return (
+    <header className='mb-10'>
+      <h1 className='text-center font-extrabold text-5xl'>Todo List</h1>
+    </header>
+  )
+}
+
+export default Haeder
