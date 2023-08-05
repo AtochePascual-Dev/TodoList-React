6533f2d41f566791f87a8b178d7918684bdc24d2
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-05 11:29:53 -1000

    [FEATURE] tarea component

---
+++ src/components/Tarea.jsx
@@ -0,0 +1,35 @@
+import IconoCheck from '../img/check.svg';
+import IconoEdit from '../img/edit.svg';
+import IconoDelete from '../img/delete.svg';
+
+const Tarea = ({ tarea }) => {
+
+  return (
+    <div className="w-11/12 mx-auto mb-5 flex justify-between items-center">
+
+      <p className='text-xl text-gray-800 font-semibold'>{tarea.tarea}</p>
+
+      <div className="flex gap-3">
+        <img
+          src={IconoCheck}
+          alt="icono de completar"
+          className='cursor-pointer'
+        />
+        <img
+          src={IconoEdit}
+          alt="icono de editar"
+          className='cursor-pointer'
+        />
+        <img
+          src={IconoDelete}
+          alt="icono de eliminar"
+          className='cursor-pointer'
+        />
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Tarea
