d575c4ca1cf88becc4fc71083149a4e29449b12c
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-05 16:24:38 -1000

    [FEATURE] localStorge

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Formulario from "./components/Formulario"
 import Haeder from "./components/Haeder"
 import ListadoTareas from "./components/ListadoTareas";
 import Select from "./components/Select";
 
 function App() {
-  const [tareas, setTareas] = useState([]);
-  const [tareasCompletas, setTareasCompletas] = useState([]);
+  const [tareas, setTareas] = useState(JSON.parse(localStorage.getItem('tareas')) || []);
+  const [tareasCompletas, setTareasCompletas] = useState(JSON.parse(localStorage.getItem('tareasCompletas')) || []);
   const [tareaEditar, setTareaEditar] = useState({});
   const [listarCompletados, setlistarCompletados] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+    localStorage.setItem('tareasCompletas', JSON.stringify(tareasCompletas));
+  }, [tareas, tareasCompletas])
 
   return (
     <div className="w-11/12 max-w-lg mt-10 mx-auto px-3 py-10 flex flex-col items-center gap-6 shadow-lg rounded-md bg-white overflow-hidden">
